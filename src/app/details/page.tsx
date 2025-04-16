c5646317909a8910d836a23da28feb8ac6d95ffe
CubeDr <spaceship00@naver.com>
2025-04-17 00:26:11 +0900

    Add responsive layout

--- src/app/details/page.tsx
+++ src/app/details/page.tsx
@@ -1,5 +1,13 @@
+import styles from './page.module.css';
+
 export default function DetailsPage() {
   return (
-    <>Details</>
+    <>
+      <div className={styles.Banner}></div>
+      <div className={styles.Body}>
+        <div className={styles.QuickStats}></div>
+        <div className={styles.Content}></div>
+      </div>
+    </>
   );
 }
