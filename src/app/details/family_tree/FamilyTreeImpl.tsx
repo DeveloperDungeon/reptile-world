0ff5e33582ff7e066d9367f6cbd71926f29894f5
CubeDr <spaceship00@naver.com>
2025-04-18 20:53:55 +0900

    Move family member rendering components to a separate directory

--- src/app/details/family_tree/FamilyTreeImpl.tsx
+++ src/app/details/family_tree/FamilyTreeImpl.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useRef, useState } from 'react';
-import FamilyMember from './FamilyMember';
+import FamilyMember from './components/FamilyMember';
 import FamilyMemberData from './FamilyMemberData';
 import styles from './FamilyTreeImpl.module.css';
 
--- src/app/details/family_tree/components/FamilyMember.tsx
+++ src/app/details/family_tree/components/FamilyMember.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import FamilyMemberData from './FamilyMemberData';
+import FamilyMemberData from '../FamilyMemberData';
 import styles from './FamilyMember.module.css';
 
 interface Props {
