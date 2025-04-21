68dee74d0cabddc490ab1513c40c40ccf4b733ec
wonbae <judiking1@naver.com>
2025-04-21 21:45:28 +0900

    Home.module.css 네임 Page.module.css로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import MainSearchForm from './MainSearchForm';
-import styles from './Home.module.css';
+import styles from './Page.module.css';
 
 export default function Home() {
   return (
@@ -7,7 +7,8 @@ export default function Home() {
       <div className={styles.textContainer}>
         <h1 className={styles.heading}>놀라운 파충류 세계 탐험</h1>
         <p className={styles.description}>
-          웅장한 도마뱀부터 화려한 뱀, 그리고 고대의 거북이까지, 매혹적인 파충류 세계를 탐험해보세요.
+          웅장한 도마뱀부터 화려한 뱀, 그리고 고대의 거북이까지, 매혹적인 파충류
+          세계를 탐험해보세요.
         </p>
       </div>
       <MainSearchForm />
