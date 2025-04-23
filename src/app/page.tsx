013d3ef381c3cc9b71cd7251aaf17592ea41b68f
CubeDr <spaceship00@naver.com>
2025-04-23 23:05:56 +0900

    Style the homepage banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,24 @@
+import BannerImage from '@/../public/gecko.jpg';
+import Image from 'next/image';
 import MainSearchForm from './MainSearchForm';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.Container}>
-      <div className={styles.TextContainer}>
-        <h1 className={styles.Heading}>놀라운 파충류 세계 탐험</h1>
-        <p className={styles.Description}>
-          웅장한 도마뱀부터 화려한 뱀, 그리고 고대의 거북이까지, 매혹적인 파충류
-          세계를 탐험해보세요.
-        </p>
+    <div className={styles.Banner}>
+      <div className={styles.ImageWrapper}>
+        <Image src={BannerImage} alt="Banner" fill className={styles.Image} />
       </div>
-      <MainSearchForm />
+      <div className={styles.BannerContent}>
+        <div className={styles.TextContainer}>
+          <h1 className={styles.Heading}>놀라운 파충류 세계 탐험</h1>
+          <p className={styles.Description}>
+            웅장한 도마뱀부터 화려한 뱀, 그리고 고대의 거북이까지, 매혹적인 파충류
+            세계를 탐험해보세요.
+          </p>
+        </div>
+        <MainSearchForm />
+      </div>
     </div>
   );
 }
