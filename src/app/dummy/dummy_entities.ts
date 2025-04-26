0e258fc0f069913f60a19d7089490e142b989ea5
GGCarry <dizzymon2@naver.com>
2025-04-26 20:25:00 +0900

    Update dummy_entities.ts
    더미데이터 및 id추가

--- src/app/dummy/dummy_entities.ts
+++ src/app/dummy/dummy_entities.ts
@@ -1,7 +1,7 @@
 import EntityData from '../data/EntityData';
 
 export const DUMMY_MALE_1: EntityData = {
-  id: '1',
+  id: 'HayunbrandgeckoM00000001',
   imageSrc:
     'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSgwy1dOJa3-jpTfRxBmXG6-u-R8IzkG_AGlKLgesk_M2I1i3K4FMA0ZWqAI5HUpZXtqmG2hhfooARUr7r8wa--VA',
   name: '게코 수컷 1',
@@ -35,7 +35,7 @@ export const DUMMY_MALE_1: EntityData = {
 };
 
 export const DUMMY_MALE_2: EntityData = {
-  id: '2',
+  id: 'HayunbrandgeckoM00000002',
   imageSrc:
     'https://zooboise.org/wp-content/uploads/2024/05/2024-Crested-gecko-block.png',
   name: '게코 수컷 2',
@@ -69,7 +69,7 @@ export const DUMMY_MALE_2: EntityData = {
 };
 
 export const DUMMY_MALE_3: EntityData = {
-  id: '3',
+  id: 'HayunbrandgeckoM00000003',
   imageSrc:
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3u6sJjAXFVCMI-2ye5DUo0eG8JjyrnvSsTsSzXbLSc8hH6Exs8RNFtQZlhSKcS-jP04c&usqp=CAU',
   name: '게코 수컷 3',
@@ -104,7 +104,7 @@ export const DUMMY_MALE_3: EntityData = {
 };
 
 export const DUMMY_MALE_4: EntityData = {
-  id: '4',
+  id: 'HayunbrandgeckoM00000004',
   imageSrc:
     'https://i0.wp.com/badgerlandrescue.com/wp-content/uploads/ToBeSorted/360_F_61097391_ynTSjFhpcXEhJCnRE7YJyG33lvBUKCO8.jpg?resize=540%2C360&ssl=1',
   name: '게코 수컷 4',
@@ -138,7 +138,7 @@ export const DUMMY_MALE_4: EntityData = {
 };
 
 export const DUMMY_MALE_5: EntityData = {
-  id: '5',
+  id: 'HayunbrandgeckoM00000005',
   imageSrc:
     'https://www.zillarules.com/-/media/project/oneweb/zilla/images/blog-articles/6-types-of-pet-geckos/crested-gecko.jpg',
   name: '게코 수컷 5',
@@ -172,8 +172,179 @@ export const DUMMY_MALE_5: EntityData = {
   },
 };
 
+export const DUMMY_MALE_6: EntityData = {
+  id: 'HayunbrandgeckoM00000006',
+  imageSrc:
+    'https://mblogthumb-phinf.pstatic.net/MjAyMTA2MjZfNzAg/MDAxNjI0NzE4OTA3MTcy.S2auuWNv59fLez28X7kmJt536NIU8eecNMkPq2J6QFsg.80lukmmDxXXq7YJs8Qk7pro5scX31eC55fCliZIddaEg.PNG',
+  name: '게코 수컷 6',
+  gender: 'MALE',
+  species: '크레스티드 게코',
+  description:
+    '뾰족한 울퉁불퉁한 체형과 화려한 색 변이로 유명한 야행성 나무도마뱀입니다. 크레스티드와 비슷하지만 조금 더 단단한 체구를 가졌습니다.',
+  details: {
+    sizeMm: {
+      min: 200,
+      max: 250,
+    },
+    lifespanDays: {
+      min: 5475, 
+      max: 7300, 
+    },
+    habitat: ['열대', '교목성'],
+    temperatureCelcius: {
+      min: 22,
+      max: 28,
+    },
+    humidity: {
+      min: 50,
+      max: 70,
+    },
+    priceWon: {
+      min: 80000,
+    },
+    difficulty: 2,
+  },
+};
+
+export const DUMMY_MALE_7: EntityData = {
+  id: 'HayunbrandgeckoM00000007',
+  imageSrc:
+    'https://mblogthumb-phinf.pstatic.net/MjAyMTA2MjZfMjMy/MDAxNjI0NzE5MTUzNDQy.Wr6kp77sPEsxkdFdC0903BWh5e3g83f-i6y8o7rdN-Yg.O74JwlpX92AZ5bcEc3RKmSo3K62gs9fUI1Suevs4pDAg.JPEG.jb-eom/%EC%95%84%EC%9E%94%ED%8B%B1.jpg?type=w800',
+  name: '게코 수컷 7',
+  gender: 'MALE',
+  species: '크레스티드 게코',
+  description:
+    '초보자에게 가장 인기가 높은 지상성 도마뱀으로, 다양한 모프와 온화한 성격이 특징입니다.',
+  details: {
+    sizeMm: {
+      min: 180,
+      max: 250,
+    },
+    lifespanDays: {
+      min: 3650,  
+      max: 7300,  
+    },
+    habitat: ['건조', '지상성'],
+    temperatureCelcius: {
+      min: 24,
+      max: 32,
+    },
+    humidity: {
+      min: 30,
+      max: 40,
+    },
+    priceWon: {
+      min: 40000,
+    },
+    difficulty: 1,
+  },
+};
+
+export const DUMMY_MALE_8: EntityData = {
+  id: 'HayunbrandgeckoM00000008',
+  imageSrc:
+    'https://mblogthumb-phinf.pstatic.net/MjAyMTA2MjZfMTk3/MDAxNjI0NzE5MjAxOTY3.fnGjm-hSrqyky1935mcHFWmS6kGHH3lwqZZ_Q3u11eAg.YCPz_XYFzZNPPFpJsP4ZWbyP5xvDFZ6UTQnf-xXjnPog.JPEG.jb-eom/%EB%B2%85%EC%8A%A4%ED%82%A8.jpg?type=w800',
+  name: '게코 수컷 8',
+  gender: 'MALE',
+  species: '크레스티드 게코',
+  description:
+    '크고 선명한 주황 반점과 카리스마 넘치는 울음소리로 유명한 도마뱀입니다. 다소 기질이 거칠어 숙련자용으로 평가됩니다.',
+  details: {
+    sizeMm: {
+      min: 300,
+      max: 400,
+    },
+    lifespanDays: {
+      min: 4015,  
+      max: 9125, 
+    },
+    habitat: ['열대', '교목성'],
+    temperatureCelcius: {
+      min: 24,
+      max: 30,
+    },
+    humidity: {
+      min: 60,
+      max: 80,
+    },
+    priceWon: {
+      min: 60000,
+    },
+    difficulty: 3,
+  },
+};
+
+export const DUMMY_MALE_9: EntityData = {
+  id: 'HayunbrandgeckoM00000009',
+  imageSrc:
+    'https://mblogthumb-phinf.pstatic.net/MjAyMTA2MjZfMTY2/MDAxNjI0NzE5MjU1MTYy.u4HJrbQBtAfAiZwsUO8PlBVYhl1HzTQQpDl033rqenkg.g8w78j4A0uHm3boP9jgR2UkxQJr9lZ2dHK8_BhKSdd8g.JPEG.jb-eom/%EB%B0%94%EC%9D%B4%EC%BB%AC%EB%9F%AC.jpg?type=w800',
+  name: '게코 수컷 9',
+  gender: 'MALE',
+  species: '크레스티드 게코',
+  description:
+    '작고 민첩하며 자연에서는 주로 암컷만 존재하지만, 소수의 수컷 개체도 확인됩니다. 사육 공간이 적게 들고 무난한 난이도를 보입니다.',
+  details: {
+    sizeMm: {
+      min: 85,
+      max: 110,
+    },
+    lifespanDays: {
+      min: 2190,  
+      max: 4380, 
+    },
+    habitat: ['열대', '교목성'],
+    temperatureCelcius: {
+      min: 23,
+      max: 27,
+    },
+    humidity: {
+      min: 60,
+      max: 80,
+    },
+    priceWon: {
+      min: 30000,
+    },
+    difficulty: 1,
+  },
+};
+
+export const DUMMY_MALE_10: EntityData = {
+  id: 'HayunbrandgeckoM00000010',
+  imageSrc:
+    'https://mblogthumb-phinf.pstatic.net/MjAyMTA2MjZfNjgg/MDAxNjI0NzE5Mjc4NDQx.MMBpB5CW67DhjrHSPRmtTO4kxii2LA49We_dArYgmYsg.VhNUpd2grjROoDELX6S6E2UuZpTpSQ6P6RxAe7NxCskg.PNG.jb-eom/%ED%83%80%EC%9D%B4%EA%B1%B0.png?type=w800',
+  name: '게코 수컷 10',
+  gender: 'MALE',
+  species: '크레스티드 게코',
+  description:
+    '둥글고 두툼한 꼬리로 지방을 저장하는 특징이 있어 “팻테일”이라는 이름이 붙었습니다. 조용하고 다루기 쉬운 성격으로 알려져 있습니다.',
+  details: {
+    sizeMm: {
+      min: 150,
+      max: 230,
+    },
+    lifespanDays: {
+      min: 3285,  
+      max: 6935,  
+    },
+    habitat: ['건조', '지상성'],
+    temperatureCelcius: {
+      min: 24,
+      max: 32,
+    },
+    humidity: {
+      min: 50,
+      max: 70,
+    },
+    priceWon: {
+      min: 45000,
+    },
+    difficulty: 1,
+  },
+};
+
+
 export const DUMMY_FEMALE_1: EntityData = {
-  id: '6',
+  id: 'HayunbrandgeckoF00000001',
   imageSrc:
     'https://jonathansjungleroadshow.co.uk/wp-content/uploads/2020/07/crestedgeckos5.jpg',
   name: '게코 암컷 1',
@@ -208,7 +379,7 @@ export const DUMMY_FEMALE_1: EntityData = {
 };
 
 export const DUMMY_FEMALE_2: EntityData = {
-  id: '7',
+  id: 'HayunbrandgeckoF00000002',
   imageSrc:
     'https://www.reptiles.swelluk.com/media/catalog/product/c/r/crested_gecko_correlophus_ciliatus.jpg?width=810&height=810&store=swell_reptiles_store_view&image-type=image',
   name: '게코 암컷 2',
@@ -243,7 +414,7 @@ export const DUMMY_FEMALE_2: EntityData = {
 };
 
 export const DUMMY_FEMALE_3: EntityData = {
-  id: '8',
+  id: 'HayunbrandgeckoF00000003',
   imageSrc:
     'https://www.reptilecentre.com/cdn/shop/articles/what_do_geckos_eat.png?v=1723822970',
   name: '게코 암컷 3',
@@ -277,7 +448,7 @@ export const DUMMY_FEMALE_3: EntityData = {
 };
 
 export const DUMMY_FEMALE_4: EntityData = {
-  id: '9',
+  id: 'HayunbrandgeckoF00000004',
   imageSrc:
     'https://www.wilbanksreptiles.com/cdn/shop/collections/BrowseCrestedGeckos.jpg?v=1711131817',
   name: '게코 암컷 4',
@@ -312,7 +483,7 @@ export const DUMMY_FEMALE_4: EntityData = {
 };
 
 export const DUMMY_FEMALE_5: EntityData = {
-  id: '10',
+  id: 'HayunbrandgeckoF00000005',
   imageSrc:
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRh4bBWj5M_BSvAh1xXwB4v0dBG1sdyNza5GE1ZK2kxSdGfPDIYZmD4QMeYPS0fFNRMWdI&usqp=CAU',
   name: '게코 암컷 5',
@@ -345,8 +516,179 @@ export const DUMMY_FEMALE_5: EntityData = {
   },
 };
 
+export const DUMMY_FEMALE_6: EntityData = {
+  id: 'HayunbrandgeckoF00000006',
+  imageSrc:
+    'https://mblogthumb-phinf.pstatic.net/MjAyMTA2MjZfMjI3/MDAxNjI0NzE5MzAwNTY2.LXRTlROg8dBnto3wi_3U8U55g-l9kVXM30RgaDLrqKIg._KH4d1xCaHPRsVM1fm_8KVblecG-wBenr6KVa_lH1BMg.PNG.jb-eom/%EB%B8%8C%EB%A6%B0%EB%93%A4.png?type=w800',
+  name: '게코 암컷 6',
+  gender: 'FEMALE',
+  species: '크레스티드 게코',
+  description:
+    '차분한 기질과 화려한 등 무늬를 가진 개체로, 초보자에게도 적합한 교목성 도마뱀입니다.',
+  details: {
+    sizeMm: { 
+      min: 185, 
+      max: 235 
+    },
+    lifespanDays: { 
+      min: 5475, 
+      max: 7300 
+    }, 
+    habitat: ['열대', '교목성'],
+    temperatureCelcius: { 
+      min: 22, 
+      max: 26 
+    },
+    humidity: { 
+      min: 60, 
+      max: 80 
+    },
+    priceWon: { 
+      min: 55000 
+    },
+    difficulty: 1,
+  },
+};
+
+export const DUMMY_FEMALE_7: EntityData = {
+  id: 'HayunbrandgeckoF00000007',
+  imageSrc:
+    'https://mblogthumb-phinf.pstatic.net/MjAyMTA2MjZfMTY3/MDAxNjI0NzE5MzMwMDQ0.Zo9etMBsXUDSSO0UickkaWT0FAQFDSQgBVQ9eZH5ThMg.I0gP2UeCApRIuX53M4jDEg_8ete17b0CDlj6TdHHp4gg.PNG.jb-eom/%ED%94%8C%EB%A0%88%EC%9E%84.png?type=w800',
+  name: '게코 암컷 7',
+  gender: 'FEMALE',
+  species: '크레스티드 게코',
+  description:
+    '다소 밝은 얼룩 패턴을 가진 모프 개체로, 야간 활동성이 두드러집니다.',
+  details: {
+    sizeMm: { 
+      min: 180, 
+      max: 225 
+    },
+    lifespanDays: { 
+      min: 5110, 
+      max: 6935 
+    }, 
+    habitat: ['열대', '교목성'],
+    temperatureCelcius: { 
+      min: 23, 
+      max: 27 
+    },
+    humidity: { 
+      min: 65, 
+      max: 80 
+    },
+    priceWon: { 
+      min: 60000 
+    },
+    difficulty: 1,
+  },
+};
+
+export const DUMMY_FEMALE_8: EntityData = {
+  id: 'HayunbrandgeckoF00000008',
+  imageSrc:
+    'https://mblogthumb-phinf.pstatic.net/MjAyMTA2MjZfMjMz/MDAxNjI0NzE5Mzc0Nzkz.WV-g5XsxX3xL97HsXG0sumvA-yVJqGiU4ObEOxMqvhsg.QgL8aGVPeWeW8OKLiCZtAT0Flr8yAPbdH2LD8PvI6HEg.PNG.jb-eom/%EC%9D%B5%ED%95%A0.png?type=w800',
+  name: '게코 암컷 8',
+  gender: 'FEMALE',
+  species: '크레스티드 게코',
+  description:
+    '도톰한 “핀 스트라이프” 볏이 잘 발달한 개체로, 번식 라인업에 자주 포함됩니다.',
+  details: {
+    sizeMm: { 
+      min: 190, 
+      max: 240 
+    },
+    lifespanDays: { 
+      min: 5475, 
+      max: 7300 
+    }, 
+    habitat: ['열대', '교목성'],
+    temperatureCelcius: { 
+      min: 22, 
+      max: 26 
+    },
+    humidity: {
+      min: 60,
+      max: 80 
+    },
+    priceWon: { 
+      min: 65000 
+    },
+    difficulty: 2,
+  },
+};
+
+export const DUMMY_FEMALE_9: EntityData = {
+  id: 'HayunbrandgeckoF00000009',
+  imageSrc:
+    'https://mblogthumb-phinf.pstatic.net/MjAyMTA2MjZfMTUg/MDAxNjI0NzE5Mzc0NzAw.GrrUV_msNF3PZfrGhUUgKvDjT-oLiVmekP8oKGjHQFog.AshxwDZpAbcPFdAE4RjcJem7JV6jYbKcn1x5hMWT9NEg.PNG.jb-eom/%ED%8A%B8%EC%9D%B5%ED%95%A0.png?type=w800',
+  name: '게코 암컷 9',
+  gender: 'FEMALE',
+  species: '크레스티드 게코',
+  description:
+    '짙은 머드 브라운 컬러 바탕에 크림색 라인 패턴이 대비를 이루는 희귀 모프입니다.',
+  details: {
+    sizeMm: { 
+      min: 195, 
+      max: 245 
+    },
+    lifespanDays: { 
+      min: 5840,
+      max: 7665 
+    }, 
+    habitat: ['열대', '교목성'],
+    temperatureCelcius: { 
+      min: 22, 
+      max: 27 
+    },
+    humidity: { 
+      min: 60,
+      max: 80 
+    },
+    priceWon: { 
+      min: 70000 
+    },
+    difficulty: 2,
+  },
+};
+
+export const DUMMY_FEMALE_10: EntityData = {
+  id: 'HayunbrandgeckoF00000010',
+  imageSrc:
+    'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FG6cRa%2FbtsAkkhC6Oj%2F4LSy5awgNLMSauWKbBm3o1%2Fimg.jpg',
+  name: '게코 암컷 10',
+  gender: 'FEMALE',
+  species: '크레스티드 게코',
+  description:
+    '“릴리 화이트” 계열의 크림빛 체색을 보이며, 활발한 먹이 반응을 자랑합니다.',
+  details: {
+    sizeMm: {
+      min: 185, 
+      max: 235 
+    },
+    lifespanDays: { 
+      min: 5475, 
+      max: 7300 
+    }, 
+    habitat: ['열대', '교목성'],
+    temperatureCelcius: { 
+      min: 22, 
+      max: 26 
+    },
+    humidity: { 
+      min: 65,
+      max: 80 
+    },
+    priceWon: { 
+      min: 90000 
+    },
+    difficulty: 2,
+  },
+};
+
+
 export const DUMMY_UNKNOWN_1: EntityData = {
-  id: '11',
+  id: 'HayunbrandgeckoU00000001',
   imageSrc:
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoDRi2eZVNfazn4JZezD2w5unH6eQbLmGpssgc9FiLWDCvOPaF_qc9YCf_YdVKwqTCuf0&usqp=CAU',
   name: '게코 6',
@@ -386,11 +728,21 @@ const DUMMY_ENTITIES: EntityData[] = [
   DUMMY_MALE_3,
   DUMMY_MALE_4,
   DUMMY_MALE_5,
+  DUMMY_MALE_6,
+  DUMMY_MALE_7,
+  DUMMY_MALE_8,
+  DUMMY_MALE_9,
+  DUMMY_MALE_10,
   DUMMY_FEMALE_1,
   DUMMY_FEMALE_2,
   DUMMY_FEMALE_3,
   DUMMY_FEMALE_4,
   DUMMY_FEMALE_5,
+  DUMMY_FEMALE_6,
+  DUMMY_FEMALE_7,
+  DUMMY_FEMALE_8,
+  DUMMY_FEMALE_9,
+  DUMMY_FEMALE_10,
   DUMMY_UNKNOWN_1,
 ];
 
