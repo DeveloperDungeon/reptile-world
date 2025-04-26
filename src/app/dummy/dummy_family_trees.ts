3577d6d008d24614ed00ec4b961186ccbf45b3d3
GGCarry <dizzymon2@naver.com>
2025-04-26 21:02:04 +0900

    Update dummy_family_trees.ts
    change order

--- src/app/dummy/dummy_family_trees.ts
+++ src/app/dummy/dummy_family_trees.ts
@@ -1,15 +1,5 @@
 import FamilyMemberData from '@/app/data/FamilyMemberData';
 import {
-  DUMMY_FEMALE_1,
-  DUMMY_FEMALE_2,
-  DUMMY_FEMALE_3,
-  DUMMY_FEMALE_4,
-  DUMMY_FEMALE_5,
-  DUMMY_FEMALE_6,
-  DUMMY_FEMALE_7,
-  DUMMY_FEMALE_8,
-  DUMMY_FEMALE_9,
-  DUMMY_FEMALE_10,
   DUMMY_MALE_1,
   DUMMY_MALE_2,
   DUMMY_MALE_3,
@@ -20,6 +10,16 @@ import {
   DUMMY_MALE_8,
   DUMMY_MALE_9,
   DUMMY_MALE_10,
+  DUMMY_FEMALE_1,
+  DUMMY_FEMALE_2,
+  DUMMY_FEMALE_3,
+  DUMMY_FEMALE_4,
+  DUMMY_FEMALE_5,
+  DUMMY_FEMALE_6,
+  DUMMY_FEMALE_7,
+  DUMMY_FEMALE_8,
+  DUMMY_FEMALE_9,
+  DUMMY_FEMALE_10,
   DUMMY_UNKNOWN_1,
 } from '@/app/dummy/dummy_entities';
 
@@ -150,6 +150,17 @@ export const DUMMY_FAMILY_TREES: FamilyMemberData[] = [
       },
     ],
   },
+  //id:HayunbrandgeckoM00000006
+  DUMMY_MALE_6,
+  //id:HayunbrandgeckoM00000007
+  DUMMY_MALE_7,
+  //id:HayunbrandgeckoM00000008
+  DUMMY_MALE_8,
+  //id:HayunbrandgeckoM00000009
+  DUMMY_MALE_9,
+  //id:HayunbrandgeckoM00000010
+  DUMMY_MALE_10,
+  
   // id: HayunbrandgeckoF00000001
   {
     ...DUMMY_FEMALE_1,
@@ -218,6 +229,16 @@ export const DUMMY_FAMILY_TREES: FamilyMemberData[] = [
       },
     ],
   },
+  //id:HayunbrandgeckoF00000006
+  DUMMY_FEMALE_6,
+  //id:HayunbrandgeckoF00000007
+  DUMMY_FEMALE_7,
+  //id:HayunbrandgeckoF00000008
+  DUMMY_FEMALE_8,
+  //id:HayunbrandgeckoF00000009
+  DUMMY_FEMALE_9,
+  //id:HayunbrandgeckoF00000010
+  DUMMY_FEMALE_10,
   // id: HayunbrandgeckoU00000001
   {
     ...DUMMY_MALE_1,
@@ -248,25 +269,5 @@ export const DUMMY_FAMILY_TREES: FamilyMemberData[] = [
     ],
   },
   
-  //id:HayunbrandgeckoM00000006
-  DUMMY_MALE_6,
-  //id:HayunbrandgeckoM00000007
-  DUMMY_MALE_7,
-  //id:HayunbrandgeckoM00000008
-  DUMMY_MALE_8,
-  //id:HayunbrandgeckoM00000009
-  DUMMY_MALE_9,
-  //id:HayunbrandgeckoM00000010
-  DUMMY_MALE_10,
-  //id:HayunbrandgeckoF00000006
-  DUMMY_FEMALE_6,
-  //id:HayunbrandgeckoF00000007
-  DUMMY_FEMALE_7,
-  //id:HayunbrandgeckoF00000008
-  DUMMY_FEMALE_8,
-  //id:HayunbrandgeckoF00000009
-  DUMMY_FEMALE_9,
-  //id:HayunbrandgeckoF00000010
-  DUMMY_FEMALE_10
   
 ];
